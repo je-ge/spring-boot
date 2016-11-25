1ecad2712aeda8b0b1e2d44627ddc8ab0357e500
1272434821@qq.com <1272434821@qq.com>
2016-11-25 23:39:40 +0800

    spring-boot-devtools

---
+++ spring-boot-devtools/src/main/java/com/jege/spring/boot/devtools/HelloController.java
@@ -0,0 +1,18 @@
+package com.jege.spring.boot.devtools;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author JE哥
+ * @email 1272434821@qq.com
+ * @description:看看devtools模块的快速
+ */
+@RestController
+public class HelloController {
+
+  @RequestMapping("/hello")
+  public String hello() {
+    return "Hello World";
+  }
+}
